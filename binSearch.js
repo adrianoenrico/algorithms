6cce592423d1b9f98200b274e44affe78ca5142b
Adriano Alecrim <adrianoenrico1@gmail.com>
2021-08-15 02:24:32 +0100

    formatting

--- binSearch.js
+++ binSearch.js
@@ -1,26 +1,30 @@
 // Very basic implementation of a binary search algorithm for finding a number's index in a given array.
-const binSearchNumArray = (n = 61, arr = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]) => {
-    arr = arr.sort((a, b) => a - b); // sort array in case it's not yet sorted
-    console.log(arr);
-    let min = 0
-    let max = arr.length - 1
-    let mid = Math.floor((arr.length - 1) / 2) // mid is the initial guess
-    while (arr[mid] != n) {
-        if (arr[mid] < n) {
-            console.debug('guess too low');
-            min = mid + 1
-        }
-        else if (arr[mid] > n) {
-            console.debug('guess too high')
-            max = mid - 1
-        }
-        console.debug('max min', max, min);
-        mid = Math.floor((max + min) / 2)
-        console.debug('mid', mid);
-        // Return -1 in case n is not in the array
-        if (!(max > min)) return -1
+const binSearchNumArray = (
+  n = 61,
+  arr = [
+    2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
+    73, 79, 83, 89, 97,
+  ]
+) => {
+  arr = arr.sort((a, b) => a - b); // sort array in case it's not yet sorted
+  console.log(arr);
+  let min = 0;
+  let max = arr.length - 1;
+  let mid = Math.floor((arr.length - 1) / 2); // mid is the initial guess
+  while (arr[mid] != n) {
+    if (arr[mid] < n) {
+      console.debug("guess too low");
+      min = mid + 1;
+    } else if (arr[mid] > n) {
+      console.debug("guess too high");
+      max = mid - 1;
     }
-    return mid
-
-}
-console.log('Your number`s index, my liege:', binSearchNumArray());
+    console.debug("max min", max, min);
+    mid = Math.floor((max + min) / 2);
+    console.debug("mid", mid);
+    // Return -1 in case n is not in the array
+    if (!(max > min)) return -1;
+  }
+  return mid;
+};
+console.log("Your number`s index, my liege:", binSearchNumArray());
