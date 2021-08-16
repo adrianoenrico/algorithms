4b71dd193e4308fd64ee814eb7a3b2c01c45a5db
Adriano Alecrim <adrianoenrico1@gmail.com>
2021-08-16 03:26:06 +0100

    refactored loop

--- insertSort.js
+++ insertSort.js
@@ -1,22 +1,10 @@
 // Inserts an element into a sorted array, while keeping it sorted.
-const insert = function (array, rightIndex, value) {
+var insert = function(array, rightIndex, value) {
     var i;
-    for (i = rightIndex; i >= 0; i--) {
-        // slides a bigger element over to the right
-        if (array[i] > value) {
-            array[i + 1] = array[i];
-            // Edge case
-            if (i == 0) {
-                array[i] = value;
-                break;
-            }
-        }
-        if (value >= array[i]) {
-            // Replaces smaller/equal element on the right
-            array[i + 1] = value;
-            break;
-        }
+    for (i = rightIndex; i >= 0 && array[i] > value; i--) {
+        array[i + 1] = array[i];
     }
+    array[i + 1] = value;
 };
 var array = [1, 2, 4, 5, 6];
 insert(array, 4, 0);
