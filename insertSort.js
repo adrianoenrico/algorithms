af82668fbdf0c9a74ebaaf1f41aaa325532815b9
Adriano Alecrim <adrianoenrico1@gmail.com>
2021-08-16 03:13:02 +0100

    reformatted

--- insertSort.js
+++ insertSort.js
@@ -1,24 +1,23 @@
 // Inserts an element into a sorted array, while keeping it sorted.
 const insert = function (array, rightIndex, value) {
-    for(var i = rightIndex; i >= 0; i--){
-        // slides a bigger element over to the right 
-        if(array[i] > value) {
+    var i;
+    for (i = rightIndex; i >= 0; i--) {
+        // slides a bigger element over to the right
+        if (array[i] > value) {
             array[i + 1] = array[i];
             // Edge case
-            if( i == 0){
+            if (i == 0) {
                 array[i] = value;
                 break;
             }
         }
-        if(value >= array[i]){
+        if (value >= array[i]) {
             // Replaces smaller/equal element on the right
             array[i + 1] = value;
             break;
         }
     }
-}
-var array = [1, 2, 4, 5, 6 ]
-insert(array, 4, 0)
+};
+var array = [1, 2, 4, 5, 6];
+insert(array, 4, 0);
 console.log(array);
-
-
